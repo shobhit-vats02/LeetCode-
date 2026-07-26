9c4f9fce4ab7d028e40fce47de1399f724d90845
SHOBHIT VATS <shobhitvats020@gmail.com>
2026-07-26 19:56:12 +0530

    Time: 1 ms (99.88%), Space: 43.3 MB (20%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char ans = 0;
+
+        for (char c : s.toCharArray())
+            ans ^= c;
+
+        for (char c : t.toCharArray())
+            ans ^= c;
+
+        return ans;
+    }
+}
