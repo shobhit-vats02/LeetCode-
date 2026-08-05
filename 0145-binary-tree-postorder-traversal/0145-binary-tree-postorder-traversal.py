95b4adff67accaf4cc046384c727b9dd8f942443
SHOBHIT VATS <shobhitvats020@gmail.com>
2026-08-05 08:41:30 +0530

    Time: 0 ms (100%), Space: 19.2 MB (68.2%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            dfs(node.right)
+            ans.append(node.val)
+
+        dfs(root)
+        return ans
