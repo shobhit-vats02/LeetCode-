fdb23d2a24270ce5f84a172f3bbc121e83513932
SHOBHIT VATS <shobhitvats020@gmail.com>
2026-08-27 23:00:17 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.23%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            // Character already mapped
+            if (map1.containsKey(ch)) {
+                if (!map1.get(ch).equals(word)) {
+                    return false;
+                }
+            }
+            // Word already mapped to another character
+            else if (map2.containsKey(word)) {
+                return false;
+            }
+            else {
+                map1.put(ch, word);
+                map2.put(word, ch);
+            }
+        }
+
+        return true;
+    }
+}
