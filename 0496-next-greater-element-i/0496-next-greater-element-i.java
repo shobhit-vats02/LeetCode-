43d987c2c2341957dbe932b81fa46dfb33aa1e23
SHOBHIT VATS <shobhitvats020@gmail.com>
2026-09-15 23:35:12 +0530

    Time: 4 ms (73.57%), Space: 45 MB (76.29%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+}
