72bf80288b2dcb64d652a8b9da5912a44a2699b5
SHOBHIT VATS <shobhitvats020@gmail.com>
2026-09-26 21:15:37 +0530

    Time: 32 ms (90.95%), Space: 90.7 MB (82.35%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        HashMap<String, String> map = new HashMap<>();
+
+        for (List<String> k : knowledge) {
+            map.put(k.get(0), k.get(1));
+        }
+
+        StringBuilder ans = new StringBuilder();
+        int i = 0;
+
+        while (i < s.length()) {
+            if (s.charAt(i) == '(') {
+                int j = i + 1;
+
+                while (s.charAt(j) != ')') {
+                    j++;
+                }
+
+                String key = s.substring(i + 1, j);
+                ans.append(map.getOrDefault(key, "?"));
+                i = j + 1;
+            } else {
+                ans.append(s.charAt(i));
+                i++;
+            }
+        }
+
+        return ans.toString();
+    }
+}
